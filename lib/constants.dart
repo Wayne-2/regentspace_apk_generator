af4bede77cea9651435a530e001c3fa05145108f
wayne <fredmon667@gmail.com>
2025-11-04 05:16:21 +0100

    Update CanvasConfig for user build

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,11 @@
 // GENERATED FILE — DO NOT EDIT MANUALLY
-// Generated on: 2025-11-02 16:29:41.601724
+// Generated on: 2025-11-04 05:16:16.887870
 
 import 'package:flutter/material.dart';
 
-/// All app theme constants for BottomNav and general use.
 class CanvasConfig {
-  static const Color appNameColor = Colors.black;
-  static const Color primaryAppTheme = Color.fromARGB(255, 243, 16, 8);
+  static const Color appNameColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color primaryAppTheme = Color.fromARGB(255, 68, 138, 255);
   static const Color iconThemeColor = Color.fromARGB(255, 255, 255, 255);
   static const Color bgColor = Color.fromARGB(255, 255, 215, 64);
   static const String selectedBgImagePath = 'assets/newbg.png';
@@ -14,4 +13,3 @@ class CanvasConfig {
   static const String appName = 'MyApp';
   static const String appIntroMessage = 'Experience retail at its peak';
 }
-
