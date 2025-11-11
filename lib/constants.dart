0ece5b541417f0eeb05c04656c1f895e2413ab2d
wayne <fredmon667@gmail.com>
2025-11-11 03:27:56 +0100

    Update CanvasConfig for user build

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,11 @@
 // GENERATED FILE — DO NOT EDIT MANUALLY
-// Generated on: 2025-11-05 15:14:35.173604
+// Generated on: 2025-11-11 03:27:51.600984
 
 import 'package:flutter/material.dart';
 
 class CanvasConfig {
   static const Color appNameColor = Color.fromARGB(255, 0, 0, 0);
-  static const Color primaryAppTheme = Color.fromARGB(255, 239, 6, 6);
+  static const Color primaryAppTheme = Color.fromARGB(255, 68, 138, 255);
   static const Color iconThemeColor = Color.fromARGB(255, 255, 255, 255);
   static const Color bgColor = Color.fromARGB(255, 255, 215, 64);
   static const String selectedBgImagePath = 'assets/newbg.png';
